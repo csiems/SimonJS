2f5d69104f37ff0600d26c1f21a1de36837e06a4
Chris Siems and Krystan Menne <csiems@gmail.com>
2016-02-15 09:12:49 -0800

    initialize jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source  = require('vinyl-source-stream');
+var jshint = require('gulp-jshint');
 
 gulp.task('jsBrowserify', function() {
   return browserify({ entries: ['./js/simon-interface.js']})
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('jshint', function() {
+  return gulp.src(['js/*.js', 'specs/specs.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 });
