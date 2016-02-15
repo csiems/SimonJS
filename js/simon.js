400e3cec7e0665fcf9d91f26e945b5a434572769
Chris Siems and Krystan Menne <kmenne@gmail.com>
2016-02-15 09:32:22 -0800

    add userGuess parameter to game objects

--- js/simon.js
+++ js/simon.js
@@ -1,7 +1,12 @@
 function Game() {
   this.sequence = [];
+  this.userGuess = [];
 }
 
 Game.prototype.getSequence = function () {
   return this.sequence;
+};
+
+Game.prototype.getUserGuess = function () {
+  return this.userGuess;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,15 @@
 describe("Game", function() {
   it("will instantiate with an empty sequence", function() {
-    var testGame = new Game();
+    var testGame = createTestGame();
     expect(testGame.getSequence()).to.be.empty; // jshint ignore:line
   });
+
+  it("will instantiate with an empty user guess array", function() {
+    var testGame = createTestGame();
+    expect(testGame.getUserGuess()).to.be.empty; // jshint ignore:line
+  });
 });
+
+var createTestGame = function() {
+  return new Game();
+};
